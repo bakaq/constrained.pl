93327595df40738ed5019f922e1ea2270b53627b
bakaq <bakaq@bakaq.net>
2023-09-16 17:50:43 -0300

    Added functor_spec/4 and (#=..)/2

--- functor_spec.pl
+++ functor_spec.pl
@@ -3,46 +3,86 @@
 */
 
 :- module(functor_spec, [
-    functor_spec/3
+    op(700, xfx, (#=..)),
+    functor_spec/3,
+    functor_spec/4,
+    (#=..)/2
 ]).
 
 :- use_module(library(atts)).
+:- use_module(library(lists)).
 :- use_module(library(debug)).
 
-:- attribute functor_spec/2.
+:- attribute functor_spec/3.
 
 functor_spec(Var, Functor, Arity) :-
     (
         (   var(Var), nonvar(Functor), nonvar(Arity)
         ;   nonvar(Var)
-        ) -> functor(Var, Functor, Arity)
-    ;   (   get_atts(Var, +functor_spec(Functor0, Arity0)) ->
-            (Functor0 = Functor, Arity0 = Arity),
+        )
+        -> functor(Var, Functor, Arity)
+    ;   (   get_atts(Var, +functor_spec(Functor0, Arity0, _)) ->
+            Functor0 = Functor,
+            Arity0 = Arity,
             (   (nonvar(Functor), nonvar(Arity)) ->
                 functor(Var, Functor, Arity)
             ;   true
             )
-        ;   put_atts(Var, +functor_spec(Functor, Arity))
+        ;   (   nonvar(Arity) ->
+                length(Args, Arity)
+            ;   true
+            ),
+            put_atts(Var, +functor_spec(Functor, Arity, Args))
         )
     ).
 
-verify_attributes(Var, Value, Goals) :-
-    (   get_atts(Var, +functor_spec(Functor, Arity)) ->
-        (   var(Value) ->
-            (   get_atts(Value, +functor_spec(Functor0, Arity0)) ->
-                (Functor0 = Functor, Arity0 = Arity)
-            ;   put_atts(Value, +functor_spec(Functor, Arity))
+functor_spec(Var, Functor, Arity, Args) :-
+    (   (   nonvar(Arity)
+        ;   nonvar(Args)
+        ) ->
+        length(Args, Arity)
+    ;   true
+    ),
+    (
+        (   var(Var), nonvar(Functor), nonvar(Args)
+        ;   nonvar(Var)
+        ) -> Var =.. [Functor|Args]
+    ;   (   get_atts(Var, +functor_spec(Functor0, Arity0, Args0)) ->
+            Functor0 = Functor,
+            Arity0 = Arity,
+            Args0 = Args,
+            (   (nonvar(Functor), nonvar(Args)) ->
+                Var =.. [Functor|Args]
+            ;   true
             )
-        ;   functor(Value, Functor, Arity)
+        ;   put_atts(Var, +functor_spec(Functor, Arity, Args))
+        )
+    ).
+
+(#=..)(Term, [Functor|Args]) :-
+        functor_spec(Term, Functor, _, Args).
+
+verify_attributes(Var, Value, Goals) :-
+    (   get_atts(Var, +functor_spec(Functor, Arity, Args)) ->
+
+        (   var(Value) ->
+            (   get_atts(Value, +functor_spec(Functor0, Arity0, Args0)) ->
+                Functor0 = Functor,
+                Arity0 = Arity,
+                Args0 = Args
+            ;   put_atts(Value, +functor_spec(Functor, Arity, Args))
+            )
+        ;   Value =.. [Functor|Args],
+            length(Args, Arity)
         ),
-        (   (nonvar(Functor), nonvar(Arity)) ->
-            Goals = [functor(Var, Functor, Arity)]
+        (   (nonvar(Functor), nonvar(Args)) ->
+            Goals = [(Var =.. [Functor|Args])]
         ;   Goals = []
         )
-    ;   Goals = []
     ).
 
 attribute_goals(Var) -->
-    { get_atts(Var, +functor_spec(Functor, Arity)) },
-    [functor_spec:functor_spec(Var, Functor, Arity)],
-    { put_atts(Var, -functor_spec(_, _)) }.
+    { get_atts(Var, +functor_spec(Functor, Arity, ArgSpec)) },
+    [functor_spec:functor_spec(Var, Functor, Arity, ArgSpec)],
+    { put_atts(Var, -functor_spec(_, _, _)) }.
+
--- tests.pl
+++ tests.pl
@@ -6,7 +6,7 @@
 
 :- use_module(functor_spec).
 
-test("functor_spec: usage as functor/3",(
+test("functor_spec/3: usage as functor/3",(
     functor_spec(A, a, 1),
     phrase(portray_clause_(A), "a(A).\n"),
 
@@ -16,12 +16,12 @@ test("functor_spec: usage as functor/3",(
     functor_spec(asdf(2,3,4), asdf, 3)
 )).
 
-test("functor_spec: general query",(
+test("functor_spec/3: general query",(
     functor_spec(A, B, C),
     phrase(portray_clause_([A, B, C]), "[A,B,C].\n")
 )).
 
-test("functor_spec: specify one of the properties",(
+test("functor_spec/3: specify one of the properties",(
     functor_spec(A, a, _),
     phrase(portray_clause_(A), "A.\n"),
 
@@ -29,19 +29,19 @@ test("functor_spec: specify one of the properties",(
     phrase(portray_clause_(B), "A.\n")
 )).
 
-test("functor_spec: complete information later",(
+test("functor_spec/3: complete information later",(
     functor_spec(A, a, _),
     phrase(portray_clause_(A), "A.\n"),
     functor_spec(A, _, 2),
     phrase(portray_clause_(A), "a(A,B).\n")
 )).
 
-test("functor_spec: conflicting information later",(
+test("functor_spec/3: conflicting information later",(
     functor_spec(A, a, _),
     \+ functor_spec(A, b, _)
 )).
 
-test("functor_spec: unification with complementary specs",(
+test("functor_spec/3: unification with complementary specs",(
     functor_spec(A, a, _),
     functor_spec(B, _, 1),
     A = B,
@@ -49,10 +49,105 @@ test("functor_spec: unification with complementary specs",(
     phrase(portray_clause_(B), "a(A).\n")
 )).
 
-test("functor_spec: unification with conflicting specs",(
+test("functor_spec/3: unification with conflicting specs",(
     functor_spec(A, a, _),
     functor_spec(B, b, _),
     \+ A = B
+)).
+
+test("functor_spec/4: general query",(
+    functor_spec(A, B, C, D),
+    phrase(portray_clause_([A, B, C, D]), "[A,B,C,D].\n")
+)).
+
+test("functor_spec/4: specify one of the properties",(
+    functor_spec(A, a, _, _),
+    phrase(portray_clause_(A), "A.\n"),
+
+    functor_spec(B, _, 2, _),
+    phrase(portray_clause_(B), "A.\n"),
+
+    functor_spec(C, _, _, [2,1]),
+    phrase(portray_clause_(C), "A.\n")
+)).
+
+test("functor_spec/4: arity and args consistency",(
+    functor_spec(_, _, 2, Aargs),
+    phrase(portray_clause_(Aargs), "[A,B].\n"),
+
+    functor_spec(_, _, Barity, [1,2]),
+    phrase(portray_clause_(Barity), "2.\n")
+)).
+
+test("functor_spec/4: complete information later",(
+    functor_spec(A, a, _, _),
+    phrase(portray_clause_(A), "A.\n"),
+    functor_spec(A, _, 2, _),
+    phrase(portray_clause_(A), "a(A,B).\n"),
+
+    functor_spec(B, a, _, _),
+    phrase(portray_clause_(B), "A.\n"),
+    functor_spec(B, _, _, [1,2]),
+    phrase(portray_clause_(B), "a(1,2).\n")
+)).
+
+test("functor_spec/4: conflicting information later",(
+    functor_spec(A, a, _, _),
+    \+ functor_spec(A, b, _, _),
+
+    functor_spec(A, _, 2, _),
+    \+ functor_spec(A, _, 3, _),
+
+    functor_spec(A, _, 2, _),
+    \+ functor_spec(A, _, _, [1,2,3]),
+
+    functor_spec(A, _, _, [_,_]),
+    \+ functor_spec(A, _, _, [_,_,_]),
+
+    functor_spec(A, _, _, [_,_]),
+    \+ functor_spec(A, _, 3, _)
+)).
+
+test("functor_spec/4: unification with complementary specs",(
+    functor_spec(A, a, _, _),
+    functor_spec(B, _, 1, _),
+    A = B,
+    phrase(portray_clause_(A), "a(A).\n"),
+    phrase(portray_clause_(B), "a(A).\n"),
+
+    functor_spec(C, a, _, _),
+    functor_spec(D, _, _, [1,2]),
+    C = D,
+    phrase(portray_clause_(C), "a(1,2).\n"),
+    phrase(portray_clause_(D), "a(1,2).\n")
+)).
+
+test("functor_spec/4: unification with conflicting specs",(
+    functor_spec(A, a, _, _),
+    functor_spec(B, b, _, _),
+    \+ A = B,
+
+    functor_spec(C, _, 2, _),
+    functor_spec(D, _, 3, _),
+    \+ C = D,
+
+    functor_spec(E, _, 2, _),
+    functor_spec(F, _, _, [_,_,_]),
+    \+ E = F,
+
+    functor_spec(G, _, _, [_,_]),
+    functor_spec(H, _, 3, _),
+    \+ G = H
+)).
+
+test("(#=..)/2: use as (=..)/2",(
+    A #=.. [a, 1, 2],
+    phrase(portray_clause_(A), "a(1,2).\n"),
+
+    a(1,2) #=.. [F|Args],
+    phrase(portray_clause_([F, Args]), "[a,[1,2]].\n"),
+
+    a(1,2) #=.. [a, 1, 2]
 )).
 
 main :-
